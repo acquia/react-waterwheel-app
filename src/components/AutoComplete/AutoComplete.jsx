8bb423130030a0173bdd828009fb94648deb22f0
Matthew Grill <matt.grill@acquia.com>
2016-12-05 15:33:56 -0800

    wip

--- src/components/AutoComplete/AutoComplete.jsx
+++ src/components/AutoComplete/AutoComplete.jsx
@@ -1,36 +1,133 @@
 import React from 'react';
+import { Link } from 'react-router';
+import moment from 'moment';
 
-import { AutoComplete, Row, Col } from 'antd';
+import { Col, Row, TreeSelect, Table, Menu, Dropdown, Icon } from 'antd';
 import './autocomplete.css';
 
 const config = require('../../config');
 const Waterwheel = require('waterwheel');
 
+const operationsMenu = (article) => {
+  const menu = (
+    <Menu>
+      <Menu.Item key="edit">
+        <Link to={`/edit/${article.id}`}>Edit</Link>
+      </Menu.Item>
+      <Menu.Item key="delete">
+        <Link to={`/delete/${article.id}`}>Delete</Link>
+      </Menu.Item>
+    </Menu>
+  );
+  const operationsButton = (
+    <Dropdown
+      onClick={(e) => console.log('dropdown.button onclick', e)}
+      overlay={menu}
+      trigger={['click']}
+    >
+      <a className="ant-dropdown-link" href="#">
+        Operations <Icon type="down" />
+      </a>
+    </Dropdown>
+  );
+  return operationsButton;
+};
+
+const ArticleTable = ({ articles, users }) => {
+  const dataSource = articles.map(article => ({
+    key: article.id,
+    title: article.attributes.title,
+    contentType: article.type.split('--')[1],
+    updated: moment.unix(article.attributes.changed).format('dddd, MMMM Do YYYY'),
+    author: users.filter(user => user.data.id === article.relationships.uid.data.id)[0].data.attributes.name,
+    operations: operationsMenu(article)
+  }));
+  const columns = [{
+    title: 'TITLE',
+    dataIndex: 'title',
+    key: 'title'
+  }, {
+    title: 'CONTENT TYPE',
+    dataIndex: 'contentType',
+    key: 'contentType'
+  }, {
+    title: 'AUTHOR',
+    dataIndex: 'author',
+    key: 'author'
+  }, {
+    title: 'UPDATE',
+    dataIndex: 'updated',
+    key: 'updated'
+  }, {
+    title: 'OPERATIONS',
+    dataIndex: 'operations',
+    key: 'operations'
+  }];
+  return (<Table
+    dataSource={dataSource}
+    columns={columns}
+    pagination={false}
+  />);
+};
+
+
 class TagAutoComplete extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasLoaded: false };
+    this.state = {
+      hasLoaded: false,
+      values: []
+    };
     this.waterwheel = new Waterwheel({ base: config.waterwheel.base, credentials: config.waterwheel.credentials });
+    this.onChange = this.onChange.bind(this);
   }
-  componentWillMount(){
-    this.waterwheel.jsonapi.get('taxonomy_term/tags', {})
-      .then(res => this.setState({
-        tags: res.data.map(tag => tag.attributes.name),
-        hasLoaded: true
-      }));
+  componentWillMount() {
+    Promise.all([
+      this.waterwheel.jsonapi.get('taxonomy_term/tags', {}),
+      this.waterwheel.jsonapi.get('node/article', { include: 'uid' })
+    ])
+      .then(res => {
+        this.setState({
+          availableTags: res[0].data.map(tag => ({
+            label: tag.attributes.name,
+            value: tag.attributes.uuid
+          })),
+          availableArticles: res[1].data,
+          renderedArticles: res[1].data,
+          users: res[1].included,
+          hasLoaded: true
+        });
+      });
   }
-  handleChange(value) {
-
+  onChange(selectedTags) {
+    this.setState({
+      renderedArticles: selectedTags.length ? this.state.availableArticles.filter(article => selectedTags.some(selectedTagId => article.relationships.field_tags.data.map(tag => tag.id).includes(selectedTagId))) : this.state.availableArticles
+    });
   }
   render() {
     return (
-      <Row>
-        {this.state.hasLoaded && <AutoComplete
-          dataSource={this.state.tags}
-          style={{ width: 200 }}
-          onChange={this.handleChange}
-        />}
-      </Row>
+      <Col>
+        {this.state.hasLoaded &&
+          <div>
+            <Row>
+              <TreeSelect
+                treeData={this.state.availableTags}
+                style={{ width: 300 }}
+                onChange={this.onChange}
+                multiple={true}
+              />
+            </Row>
+            <Row>
+              {this.state.renderedArticles.length &&
+                <ArticleTable
+                  articles={this.state.renderedArticles}
+                  users={this.state.users}
+                />
+              }
+            </Row>
+          </div>
+        }
+      </Col>
     );
   }
 }
